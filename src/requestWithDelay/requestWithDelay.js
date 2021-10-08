541e4a2b24ef096a23a7409116ca015a0a9060c1
Denis <therealpanda98@gmail.com>
2021-10-08 23:51:25 +0300

    created requestWithDelay pattern that can take care of eliminating the flickering effect.

---
+++ src/requestWithDelay/requestWithDelay.js
@@ -0,0 +1,18 @@
+export const DEFAULT_REQUEST_DELAY = 350;
+
+/**
+ * Will take care of eliminating the flickering effect when the server responds quickly.
+ *
+ * @example
+ * const getDataWithDelay = requestWithDelay(getData())
+ *
+ * @param request {Promise} - async request of data.
+ * @param delay {number} - the min time of waiting response.
+ * @returns {Promise}
+ */
+export const requestWithDelay = (request, delay = DEFAULT_REQUEST_DELAY) => {
+    return new Promise.all([
+        request,
+        new Promise(resolve => setTimeout(resolve, delay))
+    ])
+}
--- src/requestWithTimeout/requestWithTimeout.js
+++ src/requestWithTimeout/requestWithTimeout.js
@@ -1,10 +1,6 @@
 import {TimeoutError} from "./TimeoutError";
 
-export const DEFAULT_TIMEOUT = 10000;
-
-const timeoutPromise = (timeout) => new Promise((_, reject) => {
-    setTimeout(() => reject(), timeout, new TimeoutError(`Request takes longer than ${timeout} ms`));
-});
+export const DEFAULT_REQUEST_TIMEOUT = 10000;
 
 /**
  * Will cancel the request if the response from the server is longer than timeout.
@@ -16,9 +12,13 @@ const timeoutPromise = (timeout) => new Promise((_, reject) => {
  * @param timeout {number} - the upper bound for waiting for a request.
  * @returns {Promise}
  */
-export const requestWithTimeout = (request, timeout = DEFAULT_TIMEOUT) => {
+export const requestWithTimeout = (request, timeout = DEFAULT_REQUEST_TIMEOUT) => {
     return Promise.race([
         request,
-        timeoutPromise(timeout)
+        new Promise((_, reject) => setTimeout(
+            reject,
+            timeout,
+            new TimeoutError(`Request takes longer than ${timeout} ms`)
+        ))
     ])
 }

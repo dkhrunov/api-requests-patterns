6ccf4983ec4cc3430771aa7682dac831d5045cad
Denis <therealpanda98@gmail.com>
2021-10-09 00:50:29 +0300

    fixed bugs in requestWithDelay and wrote real world examples

--- src/requestWithDelay/requestWithDelay.js
+++ src/requestWithDelay/requestWithDelay.js
@@ -4,14 +4,19 @@ export const DEFAULT_REQUEST_DELAY = 350;
  * Will take care of eliminating the flickering effect when the server responds quickly.
  *
  * @example
- * const getDataWithDelay = requestWithDelay(getData());
+ * const getData = () => fetch('https://jsonplaceholder.typicode.com/posts/1');
+ *
+ * // getting response min after 3 seconds
+ * requestWithDelay(getData(), 3000)
+ *     .then(response => response.json())
+ *     .then(data => console.log(data));
  *
  * @param request {Promise} - async request of data.
  * @param delay {number} - the min time of waiting response.
  * @returns {Promise}
  */
 export const requestWithDelay = (request, delay = DEFAULT_REQUEST_DELAY) => {
-    return new Promise.all([
+    return Promise.all([
         request,
         new Promise(resolve => setTimeout(resolve, delay))
     ]).then(values => values[0]);

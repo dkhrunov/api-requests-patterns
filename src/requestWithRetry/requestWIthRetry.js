cc3f3c179224900e1a6fad033ee5d7af44f117a6
Denis <therealpanda98@gmail.com>
2021-10-09 00:48:14 +0300

    fixed bugs in requestWithRetry and wrote real world examples

--- src/requestWithRetry/requestWIthRetry.js
+++ src/requestWithRetry/requestWIthRetry.js
@@ -6,14 +6,23 @@ const exponentialBackoffStrategy = x => 2 ** x
  * Will take care of eliminating the flickering effect when the server responds quickly.
  *
  * @example
- * const getDataWithRetry = requestWithRetry(() => getData(), 3);
- * // or
+ * const getData = () => fetch('https://jsonplaceholder.typicode.com/posts/1');
+ *
+ * requestWithRetry(getData, 5)
+ *     .then(response => response.json())
+ *     .then(data => console.log(data));
+ *
+ * // with custom backoff strategy
  * const linerBackoffStrategy = x => (2 * x) + 1;
- * const getDataWithRetry = requestWithRetry(() => getData(), 3, linerBackoffStrategy);
+ *
+ * requestWithRetry(getData, 5, linerBackoffStrategy)
+ *     .then(response => response.json())
+ *     .then(data => console.log(data));
  *
  * @param request {() => Promise} - async request of data.
  * @param retries {number} - number of attempts.
  * @param strategy {(number) => number} - request rate function. Default exponential function.
+ * @param iteration {number} - **dont set this param if you wanna be correct process.**
  * @returns {Promise}
  */
 export const requestWithRetry = async (request, retries, strategy = exponentialBackoffStrategy, iteration = 0) => {
@@ -24,7 +33,7 @@ export const requestWithRetry = async (request, retries, strategy = exponentialB
     } catch (error) {
         if (retries > iteration) {
             await new Promise(resolve => setTimeout(resolve, strategy(iteration) * 1000));
-            return requestWithRetry(request, retries, strategy, iteration++);
+            return requestWithRetry(request, retries, strategy, iteration + 1);
         } else {
             throw error;
         }
